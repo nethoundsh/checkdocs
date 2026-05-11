a4f97889a71fb4a1e9a3eb11cdfa3b33ab342fe9
nethoundsh <nethound.sh@gmail.com>
2026-05-11 15:16:31 -0400

    remove research corpus from tracking, add to .gitignore

--- research/scan2.sh
+++
@@ -1,4 +0,0 @@
-for nb in $(find . -name "*.ipynb" -not -path "*/.ipynb_checkpoints/*" -not -path "./.*"); do
-  echo "=== $nb ==="
-  jq -r '[.cells[] | select(.cell_type=="code") | (.outputs // [])[] | select(.output_type == "display_data" or .output_type == "execute_result") | (.data | keys | join(","))] | join("\n")' "$nb"
-done
